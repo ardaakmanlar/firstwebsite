6dc38f6a8a265e6fe6405d66e230188ee968fd60
arda akmanlar <ardaakmanlar@outlook.com>
2025-06-09 21:59:18 +0300

    sayaç eklendi

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-document.getElementById("changeColorBtn").onclick = function () {
-  document.body.style.backgroundColor = "#d0f0c0";
+let count = 0;
+
+const counterDisplay = document.getElementById("counter");
+const increaseBtn = document.getElementById("increaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+
+increaseBtn.onclick = function () {
+  count++;
+  counterDisplay.textContent = count;
+};
+
+resetBtn.onclick = function () {
+  count = 0;
+  counterDisplay.textContent = count;
 };
